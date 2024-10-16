3d9da5d5342bd2d5a46fc731bd92179bf4363b1b
Micah Burnside <micahburnside@protonmail.com>
2024-10-16 14:14:04 -0400

    add UIViewWrapper.swift

---
+++ Sources/BridgeUI/UIViewWrapper.swift
@@ -0,0 +1,34 @@
+//
+//  UIViewWrapper.swift
+//  BridgeUI
+//
+//  Created by Micah Burnside on 10/16/24.
+//
+
+
+import SwiftUI
+import UIKit
+
+// A generic wrapper for any UIView
+public struct UIViewWrapper<UIViewType: UIView>: UIViewRepresentable {
+    
+    // Closures for creating and updating the UIView
+    private let makeUIViewClosure: () -> UIViewType
+    private let updateUIViewClosure: (UIViewType) -> Void
+    
+    // Initializer takes closures for creating and updating the UIView
+    public init(makeUIView: @escaping () -> UIViewType, updateUIView: @escaping (UIViewType) -> Void = { _ in }) {
+        self.makeUIViewClosure = makeUIView
+        self.updateUIViewClosure = updateUIView
+    }
+
+    // Creates the UIView instance for SwiftUI
+    public func makeUIView(context: Context) -> UIViewType {
+        return makeUIViewClosure()
+    }
+    
+    // Updates the UIView when SwiftUI state changes
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        updateUIViewClosure(uiView)
+    }
+}
